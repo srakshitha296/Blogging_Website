9c43cf0edcc2012098a70b2c052f132d1aba5052
srakshitha296 <srakshitha296@gmail.com>
2024-06-26 21:53:36 +0530

    view post

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from .  import views
 
 urlpatterns = [
     path('', views.list_post, name="list_post"),
-    # path('post/<int:pk>/', views.view_post, name='view_post'),
+    path('post/<int:pk>/', views.view_post, name='view_post'),
     # path('post/new/', views.add_post, name='add_post'), 
     # path('post/<int:pk>/edit/', views.edit_post,name='edit_post'),
     # path('post/<int:pk>/delete/', views.delete_post, name='delete_post'),
--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,9 @@ def list_post(request):
     blog_posts = Post.objects.all()
     return render(request, 'home.html', {'blog_posts':blog_posts})
 
-def view_post(request):
-    return render(request, 'post.html')
+def view_post(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'view_post.html', {'post':post})
 
 def add_post(request):
     return render(request, 'add.html')
