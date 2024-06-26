54e0e16eede0ce4f5ee226b152000582c32a9d8c
srakshitha296 <srakshitha296@gmail.com>
2024-06-26 21:09:42 +0530

    home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+def list_post(request):
+    blog_posts = Post.objects.all()
+    return render(request, 'home.html', {'blog_posts':blog_posts})
+
+def view_post(request):
+    return render(request, 'post.html')
+
+def add_post(request):
+    return render(request, 'add.html')
+
+def edit_post(request):
+    return render(request, 'edit.html')
+
+def delete_post(request):
+    return render(request, 'delete.html')
+
