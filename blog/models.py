1048957d4d52f223d947767abe05d57e01d19a59
srakshitha296 <srakshitha296@gmail.com>
2024-07-02 02:26:11 +0530

    completed

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
     content = models.TextField()
     author = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
